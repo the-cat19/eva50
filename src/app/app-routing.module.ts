ef76223c71cca51ebd9118ff471784c72cd4b90d
IAN CEREZO <ian.cerezo.marchant@cftmail.cl>
2024-12-19 13:26:31 -0300

    evaluacion50_2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { EncuentranosComponent } from './componentes/encuentranos/encuentranos.component';
+import { ComunicateComponent } from './componentes/comunicate/comunicate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: InicioComponent},
+  {path: "Encuentranos", component:EncuentranosComponent},
+  {path: "comunicate", component:ComunicateComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
